a96a5d311fd236af062632c76317eb485719298f
Marcoos-71 <marcos.mondejar71@gmail.com>
2025-06-20 03:57:53 +0200

    ActualizaciÃ³n automÃ¡tica

--- python world_map.py
+++ python world_map.py
@@ -1,1 +1,10 @@
-x = 1+2
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Cargar un GeoJSON de ejemplo (mapa mundial)
+url = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson"
+world = gpd.read_file(url)
+
+world.plot(figsize=(12, 8), edgecolor='black')
+plt.title('Mapa mundial básico')
+plt.show()
